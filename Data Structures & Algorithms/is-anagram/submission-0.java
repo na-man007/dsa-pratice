9b832300bf335a85bd8cb1f4f5225966ea0913fa
na-man007 <89378237+na-man007@users.noreply.github.com>
2026-05-22 06:41:24 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        return false;
+
+        int[] freq = new int[26];
+        for (int i=0;i<s.length();i++){
+            freq[s.charAt(i)-97]++;
+            freq[t.charAt(i)-97]--;
+        }
+
+        for(int x:freq){
+            if(x !=0)
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int[] twoSum(int[] nums, int target) {
+    int [] ans  = new int[2];
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i=0;i<nums.length;i++){
+        if(map.containsKey(target-nums[i])){
+            ans[0] = map.get(target-nums[i]);
+            ans[1] = i;
+            return ans;
+        }
+        else{
+            map.put(nums[i],i);
+        }
+    }
+    return ans;
+}
+}
