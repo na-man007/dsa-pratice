de4e9006911882d1f807088c5c7aa663073aae71
na-man007 <89378237+na-man007@users.noreply.github.com>
2026-07-11 22:04:37 +0530

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int i = 0;
+        while(n>0){
+            i++;
+            n-=i;
+        }
+        if(n==0)
+        return i;
+        
+        return i-1;
+    }
+}
