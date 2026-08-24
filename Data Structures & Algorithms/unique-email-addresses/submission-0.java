c0a189e8110ba925f1ef9f9398a31c5947b4f47d
na-man007 <89378237+na-man007@users.noreply.github.com>
2026-08-24 22:11:25 +0530

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> unique = new HashSet<>();
+        for(String e: emails){
+            String[] parts = e.split("@");
+            String local = parts[0];
+            String domain = parts[1];
+
+            local = local.split("\\+")[0];
+            local = local.replace(".", "");
+            unique.add(local + "@" + domain);
+        }
+        return unique.size();
+    }
+}
